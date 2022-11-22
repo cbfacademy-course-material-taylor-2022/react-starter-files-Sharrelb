6d26d682e2e71b6895aafd9d915e50aff4465bc8
Sharrelb <sharrel.browne@outlook.com>
2022-11-22 02:37:48 +0000

    Completed task 2 and 3

---
+++ jsx-components/task02/src/Form.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+function MyForm() {
+    const [name, setName] = useState("");
+    const [bio, setBio] = useState("");
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    
+      const element2 = document.getElementById("Name");
+      element2.innerHTML = name;
+
+      const element1 = document.getElementById("Bio");
+      element1.innerHTML = bio ;
+} 
+        
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>Enter your bio:
+          <input 
+            type="text" 
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    )
+  }
+
+  export default MyForm
