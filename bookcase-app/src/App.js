2282f41e4dece83938ed8dffcca4c60c99252b40
Sharrelb <sharrel.browne@outlook.com>
2022-11-24 00:45:37 +0000

    Completed task 5

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Book from './components/Book';
 import data from './models/books.json';
 
 function App() {
-  const books = data;
-  return <Book/>;
+  const [books] = useState(data);
+
+  return books.map(book => <Book key={book.id} book={book}/>);
 }
 
 export default App;
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-function Book() {
-  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
+function addBook(title){
+  console.log(`The Book '${title}' was clicked`);
 }
 
-export default Book;
+function Book({
+  book:{id,
+       saleInfo: {retailPrice},  
+       volumeInfo:{
+         title,
+         authors,
+         description,
+         imageLinks: {thumbnail},
+        }}}) {
+ 
+  return(<main>
+    <div className="book">
+      <img src={thumbnail} alt={title}/>
+    </div>
+    <div>
+      <h2>{title}</h2>
+    </div>
+    <p>by {authors ? authors.join(',') : "No Authors Listed"}</p>
+    {retailPrice && <p> £{retailPrice.amount}</p>}
+    {description && <p>{description}</p>}
+    <div> 
+    <button type="button" onClick={() => addBook(title)}>Add +</button>
+    </div>
+      
+    </main>);
+  }
+export default Book
